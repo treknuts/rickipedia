3c3966aa38fcb64a44a50bf672e78123dabf2af7
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2021-08-18 16:42:51 -0500

    Abstracted Home to it's own component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import Characters from './Characters';
+import Home from './Home';
 const axios = require('axios');
 
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://rickandmortyapi.com/api/character', { params: { page: 1 } }).then(function (response) {
-      console.log(response.data.results);
-      setCharacters(response.data.results);
-    }).catch(function (error) {
-      console.log(error);
-    });
-  }, []);
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -30,18 +20,13 @@ function App() {
         </div>  
         <div className="main-content">
           <Switch>
-            <Route exact path="/" component={this}/>
+            <Route exact path="/" component={Home}/>
             <Route path="/characters" component={Characters}/>
             <Route exact path="/episodes" component={this}/>
             <Route exact path="/Locations" component={this}/>
           </Switch>
         </div>
       </BrowserRouter>
-      
-
-      {/* <div className="content">
-        { characters.map((character) => <CharacterCard key={character.id} character={character}/> )}
-      </div> */}
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState} from 'react';
+const axios = require('axios');
+
+const Home = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character', { params: { page: 1 } }).then(function (response) {
+      console.log(response.data.results);
+      setCharacters(response.data.results);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }, []);
+
+  return (
+    <div className="content">
+        { characters.map((character) => <img key={ character.id } src={ character.image }/>)}
+    </div>
+  );
+}
+
+export default Home;
