f2c887187966dbb740f6eb8af638114dfe818644
treknuts <49407165+treknuts@users.noreply.github.com>
2021-08-18 21:02:26 -0500

    Got pagination working. Should probably put page number in URL.

--- src/Characters.js
+++ src/Characters.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './Characters.css';
 import CharacterCard from './CharacterCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 const axios = require('axios');
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/character', {
       params:
       {
-        page: 1,
+        page: currentPage,
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET, POST'
@@ -22,11 +25,25 @@ const Characters = () => {
     }).catch(function (error) {
       console.log(error);
     });
-  }, []);
+  }, [currentPage]);
+
+  const movePage = (increment) => {
+    setCurrentPage(currentPage + increment);
+  }
 
   return (
-    <div className="card-container">
-      {characters.map((character) => <CharacterCard key={character.id} character={character} />)}
+    <div>
+      <div class="pagination-row">
+        <button className="pag-button" onClick={() => movePage(-1)}>
+          <FontAwesomeIcon icon={faArrowLeft} size="2x" />
+        </button>
+        <button className="pag-button" onClick={() => movePage(1)}>
+          <FontAwesomeIcon icon={faArrowRight} size="2x" />
+        </button>
+      </div>
+      <div className="card-container">
+        {characters.map((character) => <CharacterCard key={character.id} character={character} />)}
+      </div>
     </div>
   );
 }
