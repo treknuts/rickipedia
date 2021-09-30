47db839c20ce6f68942a01e26bbd070e533a02c5
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2021-09-30 14:41:55 -0500

    Added conditional rendering to character status

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,18 +2,33 @@ import React, { useEffect } from 'react';
 import './CharacterCard.scss';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 
 
 const CharacterCard = ({ character }) => {
+    const getCharacterStatus = () => {
+        if (character.status === "Alive") {
+            return "success";
+        } else if (character.status === "Dead") {
+            return "danger";
+        } else {
+            return "secondary"
+        }
+    } 
+
     return (
-        <Card style={{ width: '20em'}}>
+        <Card style={{ width: '25em' }}>
             <Card.Img variant="left" src={character.image} className="h-auto"/>
             <Card.Body>
-                <Card.Title>{ character.name }</Card.Title>
+                <Card.Title>
+                    <div className="d-flex justify-content-between align-items-center">
+                    <h3>{ character.name }</h3>
+                    <Badge bg={getCharacterStatus()}>{ character.status }</Badge>
+                    </div>
+                </Card.Title>
                 <Card.Text>
-                    <p>{ character.status }</p>
-                    <p>{ character.gender }</p>
-                    <p>{ character.location.name }</p>
+                    <p><span className="fw-bold">Gender:</span> { character.gender }</p>
+                    <p><span className="fw-bold">Last known:</span> { character.location.name }</p>
                 </Card.Text>
                 <Button variant="success">Show More</Button>
             </Card.Body>
